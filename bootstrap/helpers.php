01c55112a2c0ab826e8eaacbf95f5c96f6e00be5
HongXunPan <15889325915@163.com>
2020-04-30 00:54:27 +0800

    default param for function

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -7,14 +7,14 @@
  */
 
 if (!function_exists('money_format_from_db')) {
-    function money_format_from_db($number , $decimals = 0 , $dec_point = '.' , $thousands_sep = ',')
+    function money_format_from_db($number , $decimals = 2 , $dec_point = '.' , $thousands_sep = ',')
     {
         return number_format(($number / 100 . ''), $decimals, $dec_point, $thousands_sep);
     }
 }
 
 if (!function_exists('money_format_to_db')) {
-    function money_format_to_db($number, $decimals = 0, $dec_point = '.' , $thousands_sep = ',')
+    function money_format_to_db($number, $decimals = 2, $dec_point = '.' , $thousands_sep = ',')
     {
         return number_format($number * 100 . '', $decimals, $dec_point, $thousands_sep);
     }
